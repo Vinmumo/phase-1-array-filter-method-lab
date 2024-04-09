fd111597ac525ffca43c8ad4381e4ecbd45e44f7
vincent <vinnymummo@gmail.com>
2024-04-09 09:32:47 +0300

    commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+function findMatching(arr , str){
+   return arr.filter(e => e.toLowerCase() === str.toLowerCase())
+}
+function fuzzyMatch(arr , str){
+    return arr.filter(e => e.toLowerCase().startsWith(str.toLowerCase()))
+}
+function matchName(obj , str){
+   return obj.filter(e => e.name === str)
+}
 // Code your solution here
